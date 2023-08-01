61ed2e827780daf4a97ce859e12d2db0f99f9f8a
iloth2000 <tranhung01a@gmail.com>
2023-08-02 01:40:18 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
             <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
                 <div class="w-full">
                     <h1 class="mb-4 text-2xl font-bold text-gray-700 dark:text-gray-200">
-                        Admin login
+                        Login
                     </h1>
                     <form action="{{ route('doLogin') }}" method="POST">
                         @csrf
